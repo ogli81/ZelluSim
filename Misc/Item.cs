bf2832772f0bfe2e2a34da3c9059d275381dc381
ogli81 <marcus@sternenschwarm.de>
2020-08-03 23:44:32 +0200

    Moved Item.cs to ZelluSim library.

---
+++ Misc/Item.cs
@@ -0,0 +1,22 @@
+﻿namespace ZelluSim.Misc
+{
+    //minor helper struct
+    public struct Item
+    {
+        public string Name { get; }
+        public string Info { get; }
+        public Item(string name, string info)
+        {
+            Name = name;
+            Info = info;
+        }
+    }
+
+    //minor helper interface
+    public interface IHasItems
+    {
+        int NumItems { get; }
+
+        Item GetItem(int index);
+    }
+}
